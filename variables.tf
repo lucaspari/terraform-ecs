5782874fd00b33ef91224e717ae3761c69881e04
lucaspari <lucas.pari@icloud.com>
2024-05-29 15:16:47 -0300

    feat: removed unused variables

--- variables.tf
+++ variables.tf
@@ -1,27 +1,3 @@
-variable "linux_image" {
-  description = "linux image Amazon"
-  type        = string
-  default     = "ami-0e242b0ebd8e91b4c"
-}
-
-variable "istance_type" {
-  description = "t2-micro"
-  type        = string
-  default     = "t2.micro"
-}
-
-variable "security_groups" {
-  description = "wizard-1"
-  type        = string
-  default     = "sg-0012776fa7fe41a93"
-}
-
-variable "security_group_http" {
-  description = "only allow HTTP"
-  type        = string
-  default     = "sg-093056215a1d87bab"
-}
-
 variable "subnet" {
   description = "my subnets"
   type        = list(string)
