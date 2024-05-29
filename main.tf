7a713f8917beb457b3167b64ab2539fac2241d42
lucaspari <lucas.pari@icloud.com>
2024-05-29 12:19:55 -0300

    feat: created policy for ecs

--- main.tf
+++ main.tf
@@ -19,5 +19,27 @@ resource "aws_ecs_cluster" "demo_ecs_cluster" {
     value = "enabled"
   }
 }
+resource "aws_iam_role" "ecs_task_execution_role" {
+  name = "ecsTaskExecutionRole"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = "sts:AssumeRole"
+        Principal = {
+          Service = "ecs-tasks.amazonaws.com"
+        }
+        Effect = "Allow"
+        Sid    = ""
+      }
+    ]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "ecs_task_execution_policy" {
+  role       = aws_iam_role.ecs_task_execution_role.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
+}
 
 
