89db8e8e304bd188c834e8fa24dcc75880fefd5b
lucaspari <lucas.pari@icloud.com>
2024-05-29 14:16:52 -0300

    feat: created ecs task

--- main.tf
+++ main.tf
@@ -1,4 +1,3 @@
-// test
 terraform {
   required_providers {
     aws = {
@@ -21,7 +20,6 @@ resource "aws_ecs_cluster" "demo_ecs_cluster" {
 }
 resource "aws_iam_role" "ecs_task_execution_role" {
   name = "ecsTaskExecutionRole"
-
   assume_role_policy = jsonencode({
     Version = "2012-10-17"
     Statement = [
@@ -42,4 +40,27 @@ resource "aws_iam_role_policy_attachment" "ecs_task_execution_policy" {
   policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
 }
 
-
+resource "aws_ecs_task_definition" "demo_task_definition" {
+  family                   = "springboot-task"
+  requires_compatibilities = ["FARGATE"]
+  runtime_platform {
+    operating_system_family = "LINUX"
+    cpu_architecture        = "ARM64"
+  }
+  network_mode       = "awsvpc"
+  cpu                = 1024
+  memory             = 2048
+  execution_role_arn = aws_iam_role.ecs_task_execution_role.arn
+  container_definitions = jsonencode([
+    {
+      name  = "springboot-service"
+      image = "docker.io/dumbleedore/helloworldexample:latest"
+      portMappings = [
+        {
+          containerPort = 8080
+          hostPort      = 8080
+        }
+      ]
+    },
+  ])
+}
