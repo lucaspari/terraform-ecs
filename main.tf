93a1cd5510f4aa304bbaaf3702b52422b752f612
lucaspari <lucas.pari@icloud.com>
2024-05-29 12:07:29 -0300

    feat: initializing cluster

---
+++ main.tf
@@ -0,0 +1,22 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "5.51.1"
+    }
+  }
+}
+provider "aws" {
+  profile = "default"
+  region  = "sa-east-1"
+}
+resource "aws_ecs_cluster" "demo_ecs_cluster" {
+  name = "demo_ecs_cluster"
+
+  setting {
+    name  = "containerInsights"
+    value = "enabled"
+  }
+}
+
+
